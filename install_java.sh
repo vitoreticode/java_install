7055cf350d66d657062ba46347a9ef092c8fa356
Vitoreti <37755730+vitoreticode@users.noreply.github.com>
2020-07-06 20:41:23 -0300

    Create install_java.sh

---
+++ install_java.sh
@@ -0,0 +1,8 @@
+# First download Oracle JDK
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.rpm
+
+# Then install JDK
+sudo yum install -y jdk-8u141-linux-x64.rpm
+
+# Verify JDK installation
+java -version
